fba429221180bd28e1ebb0f140218058b81d6642
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-11-18 11:23:04 +0900

    Allow to specify appended ignore file

--- internal/finder/finder_test.go
+++ internal/finder/finder_test.go
@@ -2,6 +2,7 @@ package finder_test
 
 import (
 	"os"
+	"reflect"
 	"sync"
 	"testing"
 
@@ -63,5 +64,56 @@ func TestFinder_string(t *testing.T) {
 	expected := "abc/dummy.log"
 	if r.Files[0] != expected {
 		t.Fatalf("Exepectd %+v, but got %+v\n", expected, r.Files[0])
+	}
+}
+
+func TestFinder_appendedIgnoreFile(t *testing.T) {
+	tempdir, err := os.MkdirTemp("", "findertest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tempdir)
+
+	testdir := tempdir + "/abc"
+	if err = os.Mkdir(testdir, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	testfile := testdir + "/dummy.log"
+	if err = os.WriteFile(testfile, []byte("Hello, world"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	ignorefile := tempdir + "/appended-ignore-file"
+	if err = os.WriteFile(ignorefile, []byte("public/"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	ignoreDirs := []string{".git", "log", "public"}
+	for _, ignoredir := range ignoreDirs {
+		dir := tempdir + "/" + ignoredir
+		if err = os.Mkdir(dir, 0755); err != nil {
+			t.Fatal(err)
+		}
+		testfile := dir + "/dummy.log"
+		if err = os.WriteFile(testfile, []byte("Hello, world\n"), 0644); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	r := &TestReplacer{}
+	f := finder.New(finder.FinderOption{Replacer: r, Pattern: "", Stdout: os.Stdout, Stderr: os.Stderr, AppendedIgnoreFile: ignorefile})
+	os.Chdir(tempdir)
+	if err = f.Run(); err != nil {
+		t.Fatalf("Run failed %+v\n", err)
+	}
+
+	if len(r.Files) != 2 {
+		t.Fatalf("Exepectd files are one, but got %+v\n", r.Files)
+	}
+
+	expected := []string{"abc/dummy.log", "appended-ignore-file"}
+	if !reflect.DeepEqual(r.Files, expected) {
+		t.Fatalf("Exepectd %+v, but got %+v\n", expected, r.Files)
 	}
 }
